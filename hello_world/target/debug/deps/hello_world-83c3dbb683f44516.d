778cce00ddd9f4e318c54f122afacc188e7a419e
9ightshade <aniekemeusanga@gmail.com>
2025-04-01 11:23:04 +0100

    hello world

---
+++ hello_world/target/debug/deps/hello_world-83c3dbb683f44516.d
@@ -0,0 +1,5 @@
+/home/nightshade/projects/Turbin3_prep/hello_world/target/debug/deps/hello_world-83c3dbb683f44516: src/main.rs
+
+/home/nightshade/projects/Turbin3_prep/hello_world/target/debug/deps/hello_world-83c3dbb683f44516.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/nightshade/projects/Turbin3_prep/hello_world/target/debug/hello_world: /home/nightshade/projects/Turbin3_prep/hello_world/src/main.rs
